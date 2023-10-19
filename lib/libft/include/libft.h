ef38a8695e2cf96c173e4e9ea8a864b5a3592764
vanichx <vanyapetrunin88@gmail.com>
2023-10-19 10:56:32 +0200

    fixed the pushing after leeaks

--- lib/libft/include/libft.h
+++ lib/libft/include/libft.h
@@ -6,7 +6,7 @@
 /*   By: eseferi <eseferi@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 16:42:09 by eseferi           #+#    #+#             */
-/*   Updated: 2023/10/18 15:20:18 by eseferi          ###   ########.fr       */
+/*   Updated: 2023/10/19 10:47:38 by eseferi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,10 @@ typedef struct s_list
 int					ft_countwords(char const *s, char c);
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
+int					ft_isspace(int c);
 int					ft_isalnum(int c);
 int					ft_isascii(int c);
 int					ft_isprint(int c);
-int					ft_isspace(int c);
 size_t				ft_strlen(const char *s);
 void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
