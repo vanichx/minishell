34a459abb79c80a49d94bbf1d5af1d9a0c244ea0
vanichx <vanyapetrunin88@gmail.com>
2023-10-20 14:59:21 +0200

    deleted printf

--- src/minishell/exit.c
+++ src/minishell/exit.c
@@ -2,8 +2,9 @@
 
 void	exit_shell(char *message, int exit_code, t_data *data)
 {
+	printf("exit_shell function\n");
+	free_data(data);
     ft_putstr_fd(message, STDERR_FILENO);
     ft_putstr_fd("\n", STDERR_FILENO);
-	free_data(data);
     exit(exit_code);
 }
