b4bb59e3fdc63839a36e3a58eeabf426e4f0e5bf
vanichx <vanyapetrunin88@gmail.com>
2023-10-30 14:16:59 +0100

    int		check_pipe_or(t_token *tmp) - fixed

--- src/lexer/token_errors1.c
+++ src/lexer/token_errors1.c
@@ -279,8 +279,18 @@ int check_red_general(t_token *tmp)
 int		check_pipe_or(t_token *tmp)
 
 {
-	if (tmp->next == NULL)
-		return (printf("minishell: syntax error near unexpected token `%s'\n", tmp->prev->word), 1);
+	if ((tmp->type == T_WORD && tmp->prev == NULL))
+	{
+		while (tmp->next->type == T_SPACE)
+		{
+			if (tmp->next->next->type == T_OR)
+				return (printf("minishell: syntax error near unexpected token `||'\n"), 1);
+			if (tmp->next->next->type == T_PIPE)
+				return (printf("minishell: syntax error near unexpected token `|'\n"), 1);
+			tmp = tmp->next;
+		}	
+		return (printf("minishell: syntax error near unexpected token `|'\n"), 1);
+	}
 	if (tmp->type == T_PIPE && (tmp->next->type == T_PIPE || tmp->next->type == T_OR))
 		return (printf("minishell: syntax error near unexpected token `|'\n"), 1);
 	if (tmp->type == T_OR && (tmp->prev == NULL || tmp->prev->type == T_PIPE))
