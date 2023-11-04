c87d7257298f9d341aa8a41bec2b14886bb32ccc
Alexandros Lappas <alappas@c1s3d13.42wolfsburg.de>
2023-11-04 23:48:03 +0100

    Small fix for input/output func

--- src/parsing/file_parse.c
+++ src/parsing/file_parse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   file_parse.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eseferi <eseferi@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*   By: alappas <alappas@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 21:01:38 by eseferi           #+#    #+#             */
-/*   Updated: 2023/11/04 21:02:27 by eseferi          ###   ########.fr       */
+/*   Updated: 2023/11/04 23:46:42 by alappas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,19 @@
 void	last_input(t_tree *tree)
 {
 	t_tree	*target_leaf;
+	t_tree	*head;
 
 	if (tree == NULL)
 		return ;
 	target_leaf = tree;
+	head = tree;
 	while (tree && tree->type != T_AND && tree->type != T_OR)
 	{
 		if (tree->type == T_RED_INP)
 			target_leaf = tree;
 		tree = tree->right;
 	}
+	head = tree;
 	while (tree && tree->type != T_AND && tree->type != T_OR)
 	{
 		tree->last_output = target_leaf->right->left;
@@ -40,16 +43,19 @@ void	last_input(t_tree *tree)
 void	last_output(t_tree *tree)
 {
 	t_tree	*target_leaf;
+	t_tree	*head;
 
 	if (tree == NULL)
 		return ;
 	target_leaf = tree;
+	head = tree;
 	while (tree && tree->type != T_AND && tree->type != T_OR)
 	{
 		if (tree->type == T_RED_OUT)
 			target_leaf = tree;
 		tree = tree->right;
 	}
+	head = tree;
 	while (tree && tree->type != T_AND && tree->type != T_OR)
 	{
 		tree->last_output = target_leaf->right->left;
