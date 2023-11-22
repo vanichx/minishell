a6e81d28674b58c4b7d29e46e7a75f3e48612cf4
vanichx <vanyapetrunin88@gmail.com>
2023-11-22 18:40:11 +0100

    echo done

--- src/execution/execute_redout.c
+++ src/execution/execute_redout.c
@@ -1,8 +1,8 @@
 #include "minishell.h"
 
-int get_output_file(t_data *data, t_tree *tree)
+int get_output_file(t_tree *tree)
 {
-	printf("Executing get_output_file function\n");
+	// printf("Executing get_output_file function\n");
 	int fd;
 	int last_fd;
 	char *file_name;
--- src/minishell/program_loop.c
+++ src/minishell/program_loop.c
@@ -6,7 +6,7 @@
 /*   By: ipetruni <ipetruni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 20:51:13 by eseferi           #+#    #+#             */
-/*   Updated: 2023/11/22 10:28:31 by ipetruni         ###   ########.fr       */
+/*   Updated: 2023/11/22 18:30:57 by ipetruni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	start_loop(t_data *data)
 		if (init_tree(data, &data->token_list))
 			continue ;
 		fix_tree(&data->tree);
-		print_tree(data->tree, 0);
+		// print_tree(data->tree, 0);
 		if (execute(data))
 			continue ;
 	}
